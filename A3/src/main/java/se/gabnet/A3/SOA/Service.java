6795893f58ea0e4a1be51723b537b7265af7e9ef
Anton <anton.holm.y@gmail.com>
2022-05-16 14:12:57 +0200

    postgres dependency and created server

--- A3/src/main/java/se/gabnet/A3/SOA/Service.java
+++ A3/src/main/java/se/gabnet/A3/SOA/Service.java
@@ -1,4 +1,16 @@
 package se.gabnet.A3.SOA;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.gabnet.A3.Shared.DatabaseAccess;
+
+import java.util.List;
+
+@RestController
 public class Service {
+
+    @GetMapping("/books")
+    public List<?> getAllBooks() {
+        return new DatabaseAccess().f_get_books();
+    }
 }
